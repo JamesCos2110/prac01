14c8c230ea1a0897a6e4e3bce34eab231d9b0a19
JamesCos2110 <cosgreavej05gmail.com>
2025-08-05 15:15:37 +0200

    testprac01

---
+++ app.js
@@ -0,0 +1,7 @@
+function sayHello(name) {
+  console.log(`Hello, ${name}`);
+}
+
+sayHello('VS Code');
+// This is a simple JavaScript function to greet the user
+// It takes a name as an argument and logs a greeting message to the console
